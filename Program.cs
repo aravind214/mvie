69f1d1b72d0978fdc15238b3a42911448eabba3e
aravind214 <83589764+aravind214@users.noreply.github.com>
2021-05-03 20:36:59 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissingNumber
+{
+    class GFG
+    {
+        /* program to test above function */
+        public static void Main()
+        {
+            int[] a = { 1, 2, 4, 5, 6 };
+            int miss = getMissingNo(a, 5);
+            Console.Write(miss);
+            Console.ReadLine();
+        }
+        // Function to ind missing number
+        static int getMissingNo(int[] a, int n)
+        {
+            int total = (n + 1) * (n + 2) / 2;
+
+            for (int i = 0; i < n; i++)
+                total -= a[i];
+
+            return total;
+        }
+
+       
+    }
+}
